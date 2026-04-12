e41ee9ca892e070d6d42ba522ebe1da79b7035f8
Ailurus-Feline <myk0629@outlook.com>
2026-04-12 22:49:35 +0800

    Add comments to DefendAction implementation

--- turn-based-combat-arena/src/action/DefendAction.java
+++ turn-based-combat-arena/src/action/DefendAction.java
@@ -7,10 +7,18 @@ import ui.GameUI;
 
 import java.util.List;
 
+/**
+ * Temporarily increases actor's defense.
+ *
+ * Applies a DefendEffect lasting for 2 turns.
+ */
 public class DefendAction implements Action {
+
     @Override
     public void execute(Combatant actor, Combatant directTarget, Player player, List<Combatant> enemies, GameUI ui) {
+
         actor.addStatusEffect(new DefendEffect(2));
+
         ui.printDefend(actor);
     }
 
